c3ad570c1aceef256d3b4ed70cac83165b4b2c62
Cheolgi kim <ksina123@naver.com>
2024-04-12 22:42:09 +0900

    로그인/회원가입페이지

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {Routes, Route, Link} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ 
+ <Routes>
+  <Route path="/login" element={<div>로그인페이지</div>}/>
+  <Route path="/signup" element={<div>회원가입페이지</div>}/>
+ </Routes>
+ </div>
   );
 }
 
