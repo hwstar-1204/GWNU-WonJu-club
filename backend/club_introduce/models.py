2a9c017ef06b4b4c83ef710364a2edad63cbec9d
hwstar-1204 <115150798+hwstar-1204@users.noreply.github.com>
2024-05-23 20:15:28 +0900

    backend Model 추가
    Club -> type 추가
    ClubDetail -> 가입방법, 위치, 활동, 회비 필드를 가진 테이블 추가

--- backend/club_introduce/models.py
+++ backend/club_introduce/models.py
@@ -15,11 +15,22 @@ class Club(models.Model):
         verbose_name_plural = '동아리들'
 
     club_name = models.CharField(max_length=20, primary_key=True)  # 동아리 이름
-    category = models.CharField(max_length=20)  # 동아리 카테고리
+    category = models.CharField(max_length=20)  # 정규, 소모임, 취업 등등
+    type = models.CharField(max_length=20)  # 운동, 음악, 종교 등등
     introducation = models.TextField()  # 동아리 소개
     photo = models.ImageField(upload_to=club_photo_directory_path, null=True)  # 동아리 사진
     logo = models.ImageField(upload_to=club_logo_directory_path, null=True)  # 동아리 로고 null-> default
     new_club = models.BooleanField(default=False)  # 신규 동아리 여부
+
+class ClubDetail(models.Model):
+    club = models.OneToOneField(Club, on_delete=models.CASCADE, primary_key=True, related_name='details')
+    join = models.CharField(max_length=100)  # 가입방법
+    location = models.CharField(max_length=100)  # 위치
+    activity = models.TextField()  # 활동
+    fee = models.CharField(max_length=100)  # 회비
+
+    def __str__(self):
+        return f"Details of {self.club.club_name}"
 
 
 #동아리 홈에서 회원을 소개하기 위한 모델
