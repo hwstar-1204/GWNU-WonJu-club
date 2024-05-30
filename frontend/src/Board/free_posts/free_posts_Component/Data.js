cc027c2ba7890efc873daeea84a73202f8482f2c
hwstar-1204 <115150798+hwstar-1204@users.noreply.github.com>
2024-05-30 17:56:47 +0900

    css 및 파일 정리
    Table 디렉토리 삭제

--- frontend/src/Board/free_posts/free_posts_Component/Data.js
+++ frontend/src/Board/free_posts/free_posts_Component/Data.js
@@ -1,69 +1,3 @@
-// Data.js
-
-// 공지사항 목록
-const noticeList = [
-  {
-    "no": 6,
-    "title": "공지사항 첫번째 게시글입니다.",
-    "content": "공지사항 첫번째 게시글 내용입니다.",
-    "createDate": "2020-10-26",
-    "readCount": 10,
-    "author": "관리자",
-    "recommendCount": 0 // 추가: 추천 수
-  },
-
-];
-
-
-// 일반 게시글 목록
-const postList = [
-  {
-    "no": 1,
-    "title": "첫번째 게시글입니다.",
-    "content": "첫번째 게시글 내용입니다.",
-    "createDate": "2020-10-25",
-    "readCount": 6,
-    "author": "작성자1",
-    "recommendCount": 0 // 추가: 추천 수
-  },
-  {
-    "no": 2,
-    "title": "두번째 게시글입니다.",
-    "content": "두번째 게시글 내용입니다.",
-    "createDate": "2020-10-25",
-    "readCount": 2,
-    "author": "작성자2",
-    "recommendCount": 0 // 추가: 추천 수
-  },
-  {
-    "no": 3,
-    "title": "세번째 게시글입니다.",
-    "content": "세번째 게시글 내용입니다.",
-    "createDate": "2020-10-25",
-    "readCount": 1,
-    "author": "작성자2",
-    "recommendCount": 0 // 추가: 추천 수
-  },
-  {
-    "no": 4,
-    "title": "네번째 게시글입니다.",
-    "content": "네번째 게시글 내용입니다.",
-    "createDate": "2020-10-25",
-    "readCount": 3,
-    "author": "작성자2",
-    "recommendCount": 0 // 추가: 추천 수
-  },
-  {
-    "no": 5,
-    "title": "다섯번째 게시글입니다.",
-    "content": "다섯번째 게시글 내용입니다.",
-    "createDate": "2020-10-25",
-    "readCount": 8,
-    "author": "작성자2",
-    "recommendCount": 0 // 추가: 추천 수
-  },
-];
-
 // 게시글 번호에 해당하는 게시글 또는 공지사항 가져오기
 const getPostByNo = async (postId,token) => {
   let url = 'http://127.0.0.1:8000/club_board/post_detail/'+postId;
@@ -165,8 +99,6 @@ export const createComment = async(post_id, content,token) => { // TODO post_id 
 
 
 export {
-  postList,
-  noticeList,
   getPostByNo,
   increaseRecommendCount
 };
