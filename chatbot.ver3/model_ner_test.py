b3c5de27c74b5f38ff31758e85dc779170fba3a4
kimyuuuuuu <helloaiworld01@gmail.com>
2024-05-31 04:08:56 +0900

    chatbot finish

--- chatbot.ver3/model_ner_test.py
+++ chatbot.ver3/model_ner_test.py
@@ -1,11 +1,11 @@
-from utils_list.Preprocess import Preprocess
-from models.ner.NerModel import NerModel
+from chatbot3_last.utils_list.Preprocess import Preprocess
+from chatbot3_last.models.ner.NerModel import NerModel
 
-p = Preprocess(word2index_dic='./train_tools/dict/chatbot_dict.bin',
-               userdic='./utils/user_dic.tsv')
+p = Preprocess(word2index_dic='./chatbot3_last/train_tools/dict/chatbot_dict.bin',
+               userdic='./chatbot3_last/utils_list/user_dic.tsv')
 
 
-ner = NerModel(model_name='./models/ner/ner_model_testNER2.h5', proprocess=p)
+ner = NerModel(model_name='./chatbot3_last/models/ner/ner_model.h5', proprocess=p)
 query = ['학습동아리에 대해 알려줘']
 
 for i in query :
